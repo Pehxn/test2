c7679f88ee1948044e9c03104816b9b6a40b12d2
Pehxn <rithmal2000@gmail.com>
2023-09-25 10:20:03 +0530

    Create hello.py
    test py project

---
+++ hello.py
@@ -0,0 +1,14 @@
+firstarray = []
+firstarray.append("car")
+firstarray.append("van")
+firstarray.append("bike")
+
+print("wanna ride a bike??")
+print(firstarray[2])
+print("")
+
+for x in firstarray:
+    print(x)
+print("")
+firstarray2 = [1,2,3]
+print(firstarray2[1])
